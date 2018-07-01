9810dc52bf06abd400d876e1f13331ce37b11763
Lyes Touati <lyes.touati@gmail.com>
2018-07-01 14:06:57 +0200

    some changes in tree script

--- tree.sh
+++ tree.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 function tr() {
-# $1 is the directory
-# $2 is the ident
+	# $1 is the directory
+	# $2 is the ident
 
-echo "$2$1"	
-local i=" == $2"
-l=$(ls $1)
-for f in $l; do
-	if [ -d "$1/$f" ]; then
-		tr "$1/${f}" "${i}"
-	else
-		echo "${i}${f}"
-	fi 
-done
+
+	f=`basename $1`
+
+	echo "$2$f"
+	local i="    $2"
+	l=$(ls $1)
+
+
+	for f in $l; do
+		if [ -d "$1/$f" ]; then
+			tr "$1/${f}" "${i}"
+		else
+			f=`basename $f`
+			echo "${i}${f}"
+		fi 
+	done
 }
 
 if [ $# -eq 1 ]; then
@@ -22,4 +28,4 @@ else
 	d="."
 fi
 
-tr $1 ""
+tr $d ""
